6e636174053d4a80ab0d921fbfe0bf98ad1d6ef0
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-10-09 21:03:53 +0200

    glömde att skicka upp det

--- obj/Debug/net8.0/moment3.AssemblyInfo.cs
+++ obj/Debug/net8.0/moment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b5cb467e521719e01edf2fbc741ca63a3e704e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71361cb1c5717f30dcf57831f661edbbd1346b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("moment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
